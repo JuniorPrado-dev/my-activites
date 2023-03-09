a42bb49f61545bfdb3497696f9a0ada6cea8c72e
Junior Prado <juniorcvnnn@gmail.com>
2023-03-09 11:39:01 -0300

    inicio

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+export default function Home() {
+ return (
+   <>
+        <Header/>
+        <Body/>
+        <Footer/>
+   </>
+ );
+}
